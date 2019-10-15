61a85fc3c82292c00bcf3c59f83448cb3c16424e
tombetthauser <tombetthauser@gmail.com>
2019-10-15 12:00:35 -0700

    Solved problem 03

--- W1D2/algorithmic_strategies_exercise/03_bubble_sort.rb
+++ W1D2/algorithmic_strategies_exercise/03_bubble_sort.rb
@@ -3,6 +3,19 @@
 # The method should return the array.
 # Do not use the built-in Array#sort
 
+def bubble_sort(arr)
+    sorted = false
+    until sorted
+        sorted = true
+        arr[0...-1].each_with_index do |ele, idx|
+            if arr[idx] > arr[idx+1]
+                arr[idx], arr[idx+1] = arr[idx+1], arr[idx]
+                sorted = false
+            end
+        end
+    end
+    arr
+end
 
 p bubble_sort([2, 8, 5, 2, 6])      # => [2, 2, 5, 6, 8]
 p bubble_sort([10, 8, 7, 1, 2, 3])  # => [1, 2, 3, 7, 8, 10]
