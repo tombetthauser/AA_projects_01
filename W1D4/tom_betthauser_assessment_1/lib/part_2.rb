614239d98461f97cca4dacbc9e4539d6128fa891
tombetthauser <tombetthauser@gmail.com>
2019-10-18 23:18:04 -0700

    Updated assessment_1 part_2

--- W1D4/tom_betthauser_assessment_1/lib/part_2.rb
+++ W1D4/tom_betthauser_assessment_1/lib/part_2.rb
@@ -1,19 +1,15 @@
-def element_count(array)
+def element_count(arr)
     hash = Hash.new(0)
-
-    array.each { |ele| hash[ele] += 1 }
-
+    arr.each { |ele| hash[ele] += 1 }
     hash
 end
 
-
-def char_replace!(str, hash)
-    str.each_char.with_index do |char, idx| 
-        str[idx] = hash[char] if hash[char]
+def char_replace!(string, hash)
+    string.each_char.with_index do |char, idx|
+        string[idx] = hash[char] if hash[char]
     end
 end
 
-
 def product_inject(arr)
-    arr.inject { |acc, ele| acc *= ele }
+    arr.inject { |acc, num| acc *= num }
 end
